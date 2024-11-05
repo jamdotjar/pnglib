a1034d1f87149d2d4dae3e09e64c34584a19f2cf
jam.jar <darkestknight3d@outlook.com>
2024-11-04 22:03:30 -0800

    Added command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,55 @@
-mod args;
-mod chunk;
-mod chunk_type;
-mod commands;
-mod png;
+#![allow(unused_variables)]
+use std::path::PathBuf;
 
-pub type Error = Box<dyn std::error::Error>;
-pub type Result<T> = std::result::Result<T, Error>;
+use structopt::StructOpt;
 
-fn main() -> Result<()> {
-    todo!()
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pngme")]
+pub enum PngMeArgs {
+    Encode(EncodeArgs),
+    Decode(DecodeArgs),
+    Remove(RemoveArgs),
+    Print(PrintArgs),
+}
+
+#[derive(StructOpt, Debug)]
+pub struct EncodeArgs {
+    input: String,
+
+    chunkType: String,
+
+    message: String,
+}
+#[derive(StructOpt, Debug)]
+pub struct DecodeArgs {
+    input: String,
+    chunkType: String,
+}
+#[derive(StructOpt, Debug)]
+pub struct RemoveArgs {
+    input: String,
+
+    chunkType: String,
+}
+#[derive(StructOpt, Debug)]
+pub struct PrintArgs {
+    input: String,
+}
+
+fn main() {
+    let args = PngMeArgs::from_args();
+    match args {
+        PngMeArgs::Encode(args) => {
+            println!("{:?}", args)
+        }
+        PngMeArgs::Decode(args) => {
+            println!("{:?}", args)
+        }
+        PngMeArgs::Remove(args) => {
+            println!("{:?}", args)
+        }
+        PngMeArgs::Print(args) => {
+            println!("{:?}", args)
+        }
+    };
 }
